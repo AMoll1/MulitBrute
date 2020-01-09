95609b8a7c4a4686c1c1197b4da74e88e053d858
user <user@master>
2020-01-09 21:47:43 +0100

    asdf

--- MultiBrute/src/Default/Test.java
+++ MultiBrute/src/Default/Test.java
@@ -9,7 +9,13 @@ public class Test {
 	
 	public static void main(String[] args) {
 
-		System.out.println("test");
+	System.out.println("####### MultiBrute by Alexejewitsch #######");
+	
+	
+	
+	
+	
+	
 	}
 
 }
